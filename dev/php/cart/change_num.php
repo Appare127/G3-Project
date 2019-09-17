3930d6795d65db6b0d0360ef70924210bdb8fd23
tiffrrr <tiffanyrong20@gmail.com>
2019-09-17 13:10:28 +0800

    'cart'

--- dev/php/cart/change_num.php
+++ dev/php/cart/change_num.php
@@ -2,13 +2,17 @@
 session_start();
 
 try{
-    $data=$_GET['num'];
-    $id=explode(",",$data)[0];
-    $num=explode(",",$data)[1];
+    if(isset($_GET['num'])){
+        $data=$_GET['num'];
+        $id=explode(",",$data)[0];
+        $num=explode(",",$data)[1];
 
-    
-    $_SESSION['cart'][$id]->num=$num;
-    echo  $_SESSION['cart'][$id]->num*$_SESSION['cart'][$id]->prodInfo[2];
+        
+        $_SESSION['cart'][$id]->num=$num;
+        echo  $_SESSION['cart'][$id]->num*$_SESSION['cart'][$id]->prodInfo[2];
+    }else{
+        
+    }
 }catch(PDOException $e){
     $errMsg = $errMsg . "錯誤訊息: " . $e->getMessage() . "</br>";
 	$errMsg .= "錯誤行號: " . $e->getLine() . "<br>";
--- dev/php/login/logout.php
+++ dev/php/login/logout.php
@@ -1,4 +1,31 @@
 <?php 
 session_start();
 session_destroy();
+
+
+// try{
+//     require_once("connectg3.php");
+//     $sql='select * from user where user_no=:user_no';
+//     //  $sql='select * from user;';
+
+//     $user=$pdo->prepare($sql);
+//     $user->bindValue(':user_no',$_POST['user_no']);
+//     $user->execute();
+//     if($user->rowCount() == 0){
+//         echo 'loginError';
+//     }else{
+//         $userRow=$user->fetchAll(PDO::FETCH_ASSOC);
+      
+//         foreach ($userRow[0] as $i => $data) {
+//             $_SESSION[$i]=$data;
+//         }
+//         echo json_encode($userRow);
+//     };
+
+
+// }catch(PDOException $e){
+//     // echo $e->getMessage();
+//     echo "sysError";
+
+// }
 ?>
