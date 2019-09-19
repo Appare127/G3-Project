9c38824ad534354cf6ece3d5274be0a07fcfc84a
nanayariougi <aa106262004@gmail.com>
2019-09-19 13:32:04 +0800

    參加選怪功能優化

--- dev/php/frank/join.php
+++ dev/php/frank/join.php
@@ -1,6 +1,7 @@
 <?php
 $errMsg = "";
 $user_no=$_GET["user_no"];
+$user_no=13;
 try {
     $dsn = "mysql:host=localhost;port=3306;dbname=dd102g3;charset=utf8";
     $user = "root";
@@ -10,7 +11,7 @@ try {
     session_start();
 //取出動物排行
     $sql_user_ctn = 
-    "select u.user_no,u.my_animal_name,u.my_animal_img, u.my_animal_bg_img
+    "select u.user_no,u.my_animal_name,u.my_animal_img, u.my_animal_bg_img,u.attend
     from user u
     where  u.user_no = {$user_no}  ";
     $user_ctn = $pdo->prepare($sql_user_ctn);
@@ -31,14 +32,21 @@ $item_user_ctnRow=0;
 while( $user_ctnRow[$item_user_ctnRow]= $user_ctn -> fetch(PDO::FETCH_ASSOC)){
    $item_user_ctnRow++;
 }
+if($user_ctnRow[0]['attend']==1){
+    echo json_encode( "error");
+}else {
+    $sql_update = "UPDATE `user` SET `attend` = '1' WHERE `user`.`user_no` = $user_no;";
+    $update = $pdo->prepare($sql_update);
+    $update ->execute();
+
 for ($i=0; $i < 1; $i++) { 
     $work_no_box=  $work_no -> fetch(PDO::FETCH_ASSOC);
     $work_no_box['work_no'] =$work_no_box['work_no']+1;
 }
 $time=date("Y-m-d");
 $sql_INSERT =
-"INSERT INTO `collections` (`work_no`, `vote`,`cmp_img`,`work_name`,`work_date`,`bg_img`,`user_no`) VALUES
-(null, 0, 'img/collections/work_{$work_no_box['work_no']}.png', '{$user_ctnRow[0]['my_animal_name']}', '{$time}', 'img/collections/work_bg_{$work_no_box['work_no']}.png', '{$user_ctnRow[0]['user_no']}');";
+"INSERT INTO `collections` (`work_no`, `vote`,`cmp_img`,`work_name`,`work_date`,`bg_img`,`user_no`,`amlbg_img`) VALUES
+(null, 0, 'img/collections/work_{$work_no_box['work_no']}.png', '{$user_ctnRow[0]['my_animal_name']}', '{$time}', 'img/collections/work_bg_{$work_no_box['work_no']}.png', '{$user_ctnRow[0]['user_no']}','img/collections/work_amlbg_{$work_no_box['work_no']}.png');";
 $data_INSERT = $pdo->prepare($sql_INSERT);
 $data_INSERT ->execute();
 
@@ -47,11 +55,11 @@ $data_INSERT ->execute();
 // (null, 0, 'img/collections/work_{$work_no_box['work_no']}.png', '{$user_ctnRow[0]['my_animal_name']}', '{$time}', 'img/collections/work_bg_{$work_no_box['work_no']}.png', 'img/collections/work_amlbg_{$work_no_box['work_no']}.png', '{$user_ctnRow[0]['user_no']}');";
 // $data_INSERT = $pdo->prepare($sql_INSERT);
 // $data_INSERT ->execute();
- copy("../../img/comtumize/user{$user_no}_aml.png", "../../img/collections/work_{$work_no_box['work_no']}.png");	
- copy("../../img/comtumize/user{$user_no}_aml_bg.png", "../../img/collections/work_bg_{$work_no_box['work_no']}.png");
- copy("../../img/comtumize/user{$user_no}_amlbg.png", "../../img/collections/work_amlbg_{$work_no_box['work_no']}.png");	
+ copy("../../img/customize/user{$user_no}_aml.png", "../../img/collections/work_{$work_no_box['work_no']}.png");	
+ copy("../../img/customize/user{$user_no}_aml_bg.png", "../../img/collections/work_bg_{$work_no_box['work_no']}.png");
+ copy("../../img/customize/user{$user_no}_amlbg.png", "../../img/collections/work_amlbg_{$work_no_box['work_no']}.png");	
 
 //sql timestamp not null
-echo json_encode( $work_no_box['work_no']);
+echo json_encode( $work_no_box['work_no']);}
 
 ?>
