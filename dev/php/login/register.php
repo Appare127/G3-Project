76723352175f341d2241b9e5b549ecd6cb9ca3f2
tiffrrr <tiffanyrong20@gmail.com>
2019-09-19 20:58:18 +0800

    lalala

--- dev/php/login/register.php
+++ dev/php/login/register.php
@@ -1,5 +1,45 @@
 <?php
+session_start();
+try{
+require_once('../connectg3.php');
+// {"id":"aaa","psw":"09871","name":"aaa","email":"1@zoo.com","ans":"qqq","hint_no":"1"}
+if(isset($_POST['data'])){
+    $data=json_decode($_POST['data']);
+    // print_r($data);
 
-echo $_REQUEST['memId'],$_REQUEST['memPsw'],$_REQUEST['email'];
+    $sql="INSERT INTO `user` (`user_no`, `user_id`, `user_psw`, `user_name`, `user_email`, `hint_answer`,  `hint_no`, `user_status`) VALUES (null,:id,:psw,:name,:email,:ans,:hint_no,1)";
 
+    
+    $user=$pdo->prepare($sql);
+    
+    foreach ($data as $i => $n) {
+        $user->bindValue(":{$i}",$n);
+    }
+    $user->execute();
+    
+
+    $user_no = $pdo->lastInsertId();
+
+    $_SESSION['user_no']=$user_no;
+   
+
+    $login=$pdo->query("select * from user where user_no = $user_no");
+    $loginRow=$login->fetch(PDO::FETCH_ASSOC);
+    
+    echo json_encode($loginRow);
+
+
+}else{
+    $qsn=$pdo->query("select * from prompts");
+    $qsnRow=$qsn->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($qsnRow);
+}
+
+
+
+}catch (PDOException $e) {
+	$errMsg = $errMsg . "錯誤訊息: " . $e->getMessage() . "</br>";
+	$errMsg .= "錯誤行號: " . $e->getLine() . "<br>";
+}
 ?>
+
