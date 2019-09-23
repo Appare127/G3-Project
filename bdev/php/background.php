e697d579ba3deca08baf0b30806f7fd60cdeeff5
Yi-ChenShelly <ychiang394@gmail.com>
2019-09-23 20:51:02 +0800

    新增背景頁面

--- bdev/php/background.php
+++ bdev/php/background.php
@@ -4,9 +4,9 @@
   try{
     require_once("connectg3.php");
 
-    $sql = "select * from tail";
-    $tails  = $pdo->query($sql);
-    $tailRows = $tails -> fetchAll(PDO::FETCH_ASSOC);
+    $sql = "select * from background";
+    $bgs  = $pdo->query($sql);
+    $bgRows = $bgs -> fetchAll(PDO::FETCH_ASSOC);
 
   }catch(PDOException $e){
     $errMsg .= "錯誤原因 : ".$e -> getMessage(). "<br>";
@@ -40,7 +40,7 @@
       <!-- Breadcrumb-->
       <ol class="breadcrumb">
         <li class="breadcrumb-item">動物部件管理</li>
-        <li class="breadcrumb-item active">動物尾巴</li>
+        <li class="breadcrumb-item active">背景管理</li>
 
       </ol>
       <div class="container-fluid">
@@ -49,43 +49,38 @@
         <div class="row">
           <div class="col-md-12">
             <div class="card">
-              <div class="card-header">動物尾巴</div>
+              <div class="card-header">背景管理</div>
 
               <div class="card-body">
                 <table class="table table-responsive-sm table-bordered">
                   <thead>
                     <tr>
-                      <th class="align-middle">尾巴編號</th>
-                      <th class="align-middle">尾巴名稱</th>
-                      <th class="align-middle" width="150">選單圖</th>
-                      <th class="align-middle" width="150">組合圖</th>
+                      <th class="align-middle">背景編號</th>
+                      <th class="align-middle">背景名稱</th>
+                      <th class="align-middle" width="150">背景圖片</th>
                       <th class="align-middle">狀態(0:下架; 1:上架)</th>
-                      <th class="align-middle">尾巴中文名稱</th>
+                      <th class="align-middle">背景中文名稱</th>
                       <th colspan="2"></th>
                     </tr>
                   </thead>
                   <tbody>
                     <!-- 新增 -->
-                    <form action="addAnimalTailData.php" method="post" enctype="multipart/form-data">
+                    <form action="addBackgroundData.php" method="post" enctype="multipart/form-data">
                         <!-- 標題列 -->
                         <tr class="tr_title">
                             <td></td>
                             <td>
-                                <input type="text" name="tail_name" id="" maxlength="10" required>
+                                <input type="text" name="bg_name" id="" maxlength="10" required>
                             </td>
                             <td>
-                                <img width="45%" src="" id="tail_img_preview">
-                                <input type="file" id="select_tail_img" name="tail_img" accept="image/*" required>
+                                <img width="45%" src="" id="bg_img_preview">
+                                <input type="file" id="select_bg_img" name="bg_img" accept="image/*" required>
                             </td>
                             <td>
-                                <img img width='50%' src="" id="tail_img_combination_preview">
-                                <input type="file" name="tail_img_combination" id="select_tail_img_combination" size="4" accept="image/*" required>
+                                <input type="number" name="bg_status" id="" size="4" required min="0" max="1">
                             </td>
                             <td>
-                                <input type="number" name="tail_status" id="" size="4" required min="0" max="1">
-                            </td>
-                            <td>
-                                <input type="text" name="tail_ch_name" id="" maxlength="10" required>
+                                <input type="text" name="bg_ch_name" id="" maxlength="10" required>
                             </td>
                             <td colspan="2">
                                 <input class="btn btn-block btn-outline-primary addbtn" type="submit" value="新增">
@@ -95,27 +90,26 @@
                     <?php
                     if( $errMsg != ""){ //例外
                             echo "<div><center>$errMsg</center></div>";
-                        }elseif($tails->rowCount()==0){
+                        }elseif($bgs->rowCount()==0){
                             echo "<div><center>無動物尾巴資料</center></div>";
                         }else{
                     ?>
 
                     <?php
                     
-                    foreach( $tailRows as $i => $tailRow){
+                    foreach( $bgRows as $i => $bgRow){
                         // echo "<pre>";
                         // print_r($haedRow);
                         // echo "</pre>";
                     ?>
                         <!-- 內容列 -->
-                        <form action="updateAnimalTailData.php" method="post" enctype="multipart/form-data">
+                        <form action="updateBackgroundData.php" method="post" enctype="multipart/form-data">
                           <tr>
-                            <td><?php echo $tailRow['tail_no'];?><input name="tail_no" type="hidden" value="<?= $tailRow['tail_no']?>"></td>
-                            <td><input type="text" name="tail_name" value="<?= $tailRow['tail_name']?>" readonly="true" class="dissinputstyle" maxlength="10" required></td>
-                            <td><img width="45%" src="../<?= $tailRow['tail_img']?>?<?php echo time();?>" alt="" class="image"><input type="file" class="tail_btnimg" name="tail_img" size="10" style="display:none" readonly="true"></td>
-                            <td><img width='50%' src="../<?= $tailRow['tail_img_combination']?>?<?php echo time();?>" alt=""><input type="file" class="combination_btnimg"name="tail_img_combination" size="10" style="display:none" readonly="true"></td>
-                            <td><input type="number" name="tail_status" value="<?= $tailRow['tail_status']?>" readonly="true" size="4" class="dissinputstyle" min="0" max="1" required></td>
-                            <td><input type="text" name="tail_ch_name" value="<?= $tailRow['tail_ch_name']?>" readonly="true" class="dissinputstyle" maxlength="10" required></td>
+                            <td><?php echo $bgRow['bg_no'];?><input name="bg_no" type="hidden" value="<?= $bgRow['bg_no']?>"></td>
+                            <td><input type="text" name="bg_name" value="<?= $bgRow['bg_name']?>" readonly="true" class="dissinputstyle" maxlength="10" required></td>
+                            <td><img width="45%" src="../<?= $bgRow['bg_img']?>?<?php echo time();?>" alt="" class="image"><input type="file" class="bg_btnimg" name="bg_img" size="10" style="display:none" readonly="true"></td>
+                            <td><input type="number" name="bg_status" value="<?= $bgRow['bg_status']?>" readonly="true" size="4" class="dissinputstyle" min="0" max="1" required></td>
+                            <td><input type="text" name="bg_ch_name" value="<?= $bgRow['bg_ch_name']?>" readonly="true" class="dissinputstyle" maxlength="10" required></td>
                             <td><input class="btn btn-block btn-outline-primary btn1" type="button" value="編輯"> </td>
                             <td><input class="btn btn-block btn-outline-primary" type="submit"  value="修改完成" disabled></td>
                           </form>
@@ -159,15 +153,12 @@
       updatetr.children[1].firstChild.removeAttribute("readonly");   
       updatetr.children[2].firstChild.removeAttribute("readonly"); 
       updatetr.children[2].lastChild.style.display='block';   
-      updatetr.children[3].firstChild.removeAttribute("readonly");
-      updatetr.children[3].lastChild.style.display='block';   
+      updatetr.children[3].firstChild.removeAttribute("readonly");    
       updatetr.children[4].firstChild.removeAttribute("readonly");    
-      updatetr.children[5].firstChild.removeAttribute("readonly");
       updatetr.children[1].firstChild.classList.remove("dissinputstyle");
       updatetr.children[3].firstChild.classList.remove("dissinputstyle");
       updatetr.children[4].firstChild.classList.remove("dissinputstyle");
-      updatetr.children[5].firstChild.classList.remove("dissinputstyle");
-      updatetr.children[7].firstChild.removeAttribute("disabled");
+      updatetr.children[6].firstChild.removeAttribute("disabled");
     }
       
     var btn1= document.getElementsByClassName('btn1');
@@ -182,15 +173,15 @@
 
     // 控制新增選單圖片時的預覽
     window.addEventListener("load", function () {
-      $id("tail_img_preview").style.display="none";   
-      $id("select_tail_img").onchange = function (e) {
+      $id("bg_img_preview").style.display="none";   
+      $id("select_bg_img").onchange = function (e) {
           let file = e.target.files[0];
 
           let reader = new FileReader(); //建立新的 FileReader 物件
           reader.onload = function (e) {
 
-            $id("tail_img_preview").style.display="block";   
-            $id("tail_img_preview").src = reader.result;
+            $id("bg_img_preview").style.display="block";   
+            $id("bg_img_preview").src = reader.result;
           }
 
           reader.readAsDataURL(file);
@@ -199,17 +190,17 @@
     
     // 控制新增組合圖片時的預覽
     window.addEventListener("load", function () {
-      $id("tail_img_combination_preview").style.display="none";   
-      $id("select_tail_img_combination").onchange = function (e) {
+      $id("bg_img_combination_preview").style.display="none";   
+      $id("select_bg_img_combination").onchange = function (e) {
           let file = e.target.files[0];
 
           let reader = new FileReader(); //建立新的 FileReader 物件
           reader.onload = function (e) {
 
             // 執行onchange事件後才會顯示
-            $id("tail_img_combination_preview").style.display="block";
+            $id("bg_img_combination_preview").style.display="block";
 
-            $id("tail_img_combination_preview").src = reader.result;
+            $id("bg_img_combination_preview").src = reader.result;
           }
 
           reader.readAsDataURL(file);
@@ -217,7 +208,7 @@
     })
 
     // 控制修改選單圖片時的預覽
-    var btnimg=document.getElementsByClassName('tail_btnimg');
+    var btnimg=document.getElementsByClassName('bg_btnimg');
 
     function changeImg(e){
       let file = e.target.files[0];
